2d9db3b1046e5e31418bbc6e38e60ca3a32ec949
hyoinjeong <hyoin.jeong3@gmail.com>
2023-08-02 17:09:09 +0900

    Fix: CORS 수정

--- src/main/java/com/ssafy/drug/config/MvcConfiguration.java
+++ src/main/java/com/ssafy/drug/config/MvcConfiguration.java
@@ -1,4 +1,4 @@
-package com.example.gccoffee.configuration;
+package com.ssafy.drug.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
