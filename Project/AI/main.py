fe6d1f3c37b445196a38e89dbbd63ffedb9cf780
hyoinjeong <hyoin.jeong3@gmail.com>
2023-08-03 17:57:45 +0900

    Feat: 마약후 AI 프로젝트 불러오기(미완성)

--- Project/AI/main.py
+++ Project/AI/main.py
@@ -1,6 +1,8 @@
 from flask import Flask, request,jsonify
 from werkzeug.utils import secure_filename
+from flask import send_file
 import os
+from models.e4e import predict
 
 def remo_credir():
     try:
@@ -24,9 +26,18 @@ app.config['UPLOAD_FOLDER'] = 'uploaded\\image' #경로설정
 def pred():
     if request.method == 'POST':
         remo_credir()
+        # 입력받은 사용자 사진 저장
         f = request.files['file']
         f.save(os.path.join(app.config['UPLOAD_FOLDER'], secure_filename(f.filename)))
-        return "저장성공"
+        
+        # GAN 적용
+        
+        # 입력받은 사용자 사진 삭제
+        
+        # 결과 이미지 반환
+        image_path = 'result\\' + 'apple.jpg'
+        predict()
+        return send_file(image_path, mimetype='image/jpeg')
     if request.method == 'GET':
         return "get!"
 
